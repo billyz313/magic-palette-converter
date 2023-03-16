bb91623914f029825d2ba149b27440dee7111a8e
Billy Ashmall <billy.ashmall@nasa.gov>
2023-03-16 11:32:40 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-"# magic-palette-converter" 
+# Magic Palette Converter for THREDDS 5.x
+
+[![Python: 3.9](https://img.shields.io/badge/python-3.9-blue.svg)](https://www.python.org/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+This script is used primarily for converting palette files from RGB values to hex values.  In THREDDS V4.8 and below 
+the palettes were defined by either RGB values or RGB percentages.  In THREDDS V5.x they require the palettes 
+to be hex values.
+To make the conversion an easier process I have created the Magic Palette Converter for THREDDS.
+
+## Use
+- Clone the script
+- run the script
+
+### Quick note before running the script
+This script will overwrite existing palettes which usually is the desired outcome due to 
+naming, however if you would like to preserve the original palettes you can pass -o False and new palettes will be 
+created with the prefix hex_
+
+When running pass -p with the full system path to your palettes.  Relative paths from the python file usually 
+work, but full paths always work.
+
+```shell
+python magic_palette_converter.py -path PATH_TO_YOUR_PALETTES
+```
+
+If there were any palette files that could not be converted they will be noted in the console.  This could
+happen if there are unusual lines of text in the files.  If you find a case where this happens, please let
+me know, so I can add logic to fix such a case.
+
+## Contact
+
+### Authors
+
+- [Billy Ashmall](https://github.com/billyz313)
+
+
+## License and Distribution
+
+Magic Palette Converter for THREDDS is distributed by Billy Ashmall under the terms of the MIT License. See
+[LICENSE](https://github.com/billyz313/magic-palette-converter/blob/master/LICENSE) in this directory for more 
+information.
+
+## Privacy & Terms of Use
+
+Magic Palette Converter for THREDDS in no way collects or uses any personal information.  This is completely 
+open source software (OSS), you are free to  use this software as you find it useful to your tasks.  
+Feel free to modify, update, and submit a pull request.
--- magic_palette_converter.py
+++ magic_palette_converter.py
@@ -1,16 +1,30 @@
-"""
-Place palettes in a subdirectory named palettes.  Place this file in the parent directory.
-run python magic_palette_converter.py
-"""
 import os
+import argparse
+import re
+
+
+def parse_arguments():
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-path",
+                        "--palette_path",
+                        help="Path to palettes you need converted",
+                        default=os.path.join(os.getcwd(), "palettes"))
+    parser.add_argument("-o",
+                        "--overwrite",
+                        help="Overwrite is true by default, to maintain current palette files and create new ones set "
+                             "to False",
+                        default="True")
+
+    return parser.parse_args()
 
 
 def convert_percent(which):
     return float(which) * 255
 
 
-def convert_to_hex(file_name):
-    with open(os.path.join("palettes", file_name), 'r+') as file:
+def convert_to_hex(palette_path, file_name, _overwrite):
+    with open(os.path.join(palette_path, file_name), 'r+') as file:
         # Read each line of the file
         lines = file.readlines()
 
@@ -21,7 +35,7 @@ def convert_to_hex(file_name):
                 if lines[i].startswith("#") or lines[i].strip() == "":
                     pass
                 else:
-                    the_split = lines[i].replace('\t', ' ').replace('   ', ' ').replace('  ', ' ').strip().split(' ')
+                    the_split = re.sub('[ \t]+', ' ', lines[i]).strip().split(' ')
                     r = the_split[0]
                     g = the_split[1]
                     b = the_split[2]
@@ -35,21 +49,34 @@ def convert_to_hex(file_name):
 
                     # Overwrite the RGB values with the hex value
                     lines[i] = hex_value + '\n'
-            except:
+            except Exception as e:
                 print("Could not process: " + file_name)
+                print(str(e))
                 lines = []
                 break
 
         # Write the updated lines back to the file
         if len(lines) > 0:
-            file.seek(0)
-            file.truncate()
-            file.writelines(lines)
+            if _overwrite:
+                file.seek(0)
+                file.truncate()
+                file.writelines(lines)
+            else:
+                with open(os.path.join(palette_path, "hex_" + file_name), 'w') as hex_file:
+                    hex_file.writelines(lines)
 
 
-cwd = os.path.join(os.getcwd(), "palettes")
-for filename in os.listdir(cwd):
-    # Check if the file is a text file
-    if filename.endswith('.pal'):
-        # Call the function to convert RGB values to hex and overwrite the file
-        convert_to_hex(filename)
+if __name__ == '__main__':
+    # Parse the arguments
+    args = parse_arguments()
+
+    print("You are converting your palettes from: " + args.palette_path)
+    overwrite = args.overwrite.lower().capitalize() == "True"
+
+    for filename in os.listdir(args.palette_path):
+        # Check if the file is a text file
+        if filename.endswith('.pal'):
+            # Call the function to convert RGB values to hex and overwrite the file
+            convert_to_hex(args.palette_path, filename, overwrite)
+
+    print("Your palette conversion is complete.")
